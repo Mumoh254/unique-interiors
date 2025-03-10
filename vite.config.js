3874cd5ed765c4a2bdbc29e5fd35c8a241735a83
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2025-03-10 00:10:47 -0700

    fixing   refresh   issue  in production  environment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    
-  ]
+  plugins: [react()],
+  server: {
+    historyApiFallback: true, // Fixes page refresh issue
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
 });
