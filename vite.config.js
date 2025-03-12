22016cbaf4af93bcb7d1f31f398b1e1d9b7944fe
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2025-03-12 04:17:25 -0700

    alogarith  fixes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [
+    react(),
+    compression({
+      ext: '.gz', // Use Gzip (Brotli can cause issues)
+      algorithm: 'gzip',
+      threshold: 1024,
+    }),
+  ],
   server: {
     historyApiFallback: true,
   },
