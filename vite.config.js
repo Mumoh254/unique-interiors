929b2ba09347022d1617e094ac4c0f28130cfb12
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2025-03-11 23:14:01 -0700

    setting  up  middlewareware  to  handle  refreshs

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
- base: '/',
+  base: '/',
   server: {
-    historyApiFallback: true,
+    middlewareMode: 'html', // Ensure fallback works
   },
 });
