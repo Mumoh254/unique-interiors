b68bde3965e9d027d5fef118714fb709c58e8c77
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2025-03-11 23:32:22 -0700

    fixing  browser router  duplicates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React, { Suspense, lazy } from 'react';
-import {  Routes, Route, Navigate } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Layout from './layout/layout';
 import LoadingSpinner from './pages/looder'; 
-import ScrollToTop from "./pages/scrollTop";
+import ScrollToTop from './pages/scrollTop';
 
-// Lazy load pages
 const Home = lazy(() => import('./pages/home'));
 const Quote = lazy(() => import('./pages/quote'));
 const Consultation = lazy(() => import('./pages/consoltation'));
@@ -18,37 +16,31 @@ const Projects = lazy(() => import('./pages/projects'));
 const TermsOfService = lazy(() => import('./pages/terms'));
 const CookiesPolicy = lazy(() => import('./pages/cookies'));
 const ProcessComponent = lazy(() => import('./pages/process'));
-const Team  =  lazy(() => import('./pages/team') )
+const Team = lazy(() => import('./pages/team'));
 
 function App() {
   return (
-    <BrowserRouter>
-   
-      <Suspense fallback={<LoadingSpinner />}> 
+    <Suspense fallback={<LoadingSpinner />}> 
       <ScrollToTop />
-        <Routes>
-        
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="quote" element={<Quote />} />
-            <Route path="about" element={<About />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="cookies" element={<CookiesPolicy />} />
-            <Route path="process" element={<ProcessComponent />} />
-            <Route path="faq" element={<FAQ />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="terms" element={<TermsOfService />} />
-            <Route path="team" element={<Team />} />
-            <Route path="consultation" element={<Consultation />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="quote" element={<Quote />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="cookies" element={<CookiesPolicy />} />
+          <Route path="process" element={<ProcessComponent />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="terms" element={<TermsOfService />} />
+          <Route path="team" element={<Team />} />
+          <Route path="consultation" element={<Consultation />} />
 
-            {/*  404 page */}
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-
-      </Suspense>
-  
-    </BrowserRouter>
+          {/* 404 page */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
